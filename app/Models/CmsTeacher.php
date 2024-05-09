ec51b31c16b0b7b7d11e213be3f7e47c2ef07fb7
Hancie123 <hanciewanemphago@gmail.com>
2024-05-09 23:32:39 +0545

    cms teacher added

--- app/Models/CmsTeacher.php
+++ app/Models/CmsTeacher.php
@@ -6,9 +6,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class CmsTeacher extends BaseModel implements HasMedia
 {
     use HasFactory;
     use InteractsWithMedia;
+
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this->addMediaConversion('cms_teacher_thumb')
+            ->width(450)
+            ->height(450);
+    }
 }
