0b56e3470c1ebfd6dcb33672e4bfedeaaba3792a
Hancie123 <hanciewanemphago@gmail.com>
2024-06-29 12:52:52 +0545

    monthly fees particular updated

--- resources/views/admin_layouts/sidebar.blade.php
+++ resources/views/admin_layouts/sidebar.blade.php
@@ -10,10 +10,14 @@
                             @lang('translation.dashboard')</span></a>
 
                 </li>
-                <li class="{{ request()->is('admin/users*') ? 'active' : '' }}">
-                    <a href="{{ url('admin/users') }}"><i class='bx bxs-user'></i> <span> User Management</span></a>
 
-                </li>
+                @if (Auth()->user()->role == 'admin')
+                    <li class="{{ request()->is('admin/users*') ? 'active' : '' }}">
+                        <a href="{{ url('admin/users') }}"><i class='bx bxs-user'></i> <span> User Management</span></a>
+
+                    </li>
+                @endif
+
 
                 <li class="submenu {{ request()->is('admin/cms*') ? 'active' : '' }}">
                     <a href="#"><i class="fas fa-book"></i> <span> @lang('translation.cms')</span> <span
@@ -116,7 +120,8 @@
                                 class="{{ request()->is('admin/bank-book') ? 'active' : '' }}">Bank
                                 Book</a></li>
                         <li><a href="{{ url('admin/bank-book/create-bank-account') }}"
-                                class="{{ request()->is('admin/bank-book/create-bank-account') ? 'active' : '' }}">Register New Bank</a></li>
+                                class="{{ request()->is('admin/bank-book/create-bank-account') ? 'active' : '' }}">Register
+                                New Bank</a></li>
 
                     </ul>
                 </li>
@@ -126,37 +131,6 @@
 
                 </li>
 
-
-
-                {{-- <li class="submenu {{ request()->is('admin/tools*') ? 'active' : '' }}">
-                    <a href="#"><i class="feather-box"></i> <span> Staff Management</span> <span
-                            class="menu-arrow"></span></a>
-                    <ul>
-                        <li><a href="{{ url('admin/tools/calendar') }}"
-                                class="{{ request()->is('admin/tools/calendar') ? 'active' : '' }}">Calendar</a></li>
-                        <li><a href="{{ url('admin/tools/nepali-date-converter') }}"
-                                class="{{ request()->is('admin/tools/nepali-date-converter') ? 'active' : '' }}">Nepali
-                                Date Converter</a></li>
-
-
-                    </ul>
-                </li>
-
-               
-
-                <li class="submenu {{ request()->is('admin/tools*') ? 'active' : '' }}">
-                    <a href="#"><i class="feather-box"></i> <span> Graph Reports</span> <span
-                            class="menu-arrow"></span></a>
-                    <ul>
-                        <li><a href="{{ url('admin/tools/calendar') }}"
-                                class="{{ request()->is('admin/tools/calendar') ? 'active' : '' }}">Calendar</a></li>
-                        <li><a href="{{ url('admin/tools/nepali-date-converter') }}"
-                                class="{{ request()->is('admin/tools/nepali-date-converter') ? 'active' : '' }}">Nepali
-                                Date Converter</a></li>
-
-
-                    </ul>
-                </li> --}}
 
                 <li class="submenu {{ request()->is('admin/tools*') ? 'active' : '' }}">
                     <a href="#"><i class="bi bi-tools"></i> <span> Tools</span> <span
@@ -171,62 +145,6 @@
 
                     </ul>
                 </li>
-                {{-- 
-                <li class="submenu">
-                    <a href="#"><i class="fas fa-book-reader"></i> <span> Subjects</span> <span
-                            class="menu-arrow"></span></a>
-                    <ul>
-                        <li><a href="subjects.html">Subject List</a></li>
-                        <li><a href="add-subject.html">Subject Add</a></li>
-                        <li><a href="edit-subject.html">Subject Edit</a></li>
-                    </ul>
-                </li>
-                <li class="submenu">
-                    <a href="#"><i class="fas fa-clipboard"></i> <span> Invoices</span> <span
-                            class="menu-arrow"></span></a>
-                    <ul>
-                        <li><a href="invoices.html">Invoices List</a></li>
-                        <li><a href="invoice-grid.html">Invoices Grid</a></li>
-                        <li><a href="add-invoice.html">Add Invoices</a></li>
-                        <li><a href="edit-invoice.html">Edit Invoices</a></li>
-                        <li><a href="view-invoice.html">Invoices Details</a></li>
-                        <li><a href="invoices-settings.html">Invoices Settings</a></li>
-                    </ul>
-                </li>
-
-
-                <li class="menu-title">
-                    <span>Management</span>
-                </li>
-                
-                <li>
-                    <a href="holiday.html"><i class="fas fa-holly-berry"></i> <span>Holiday</span></a>
-                </li>
-                <li>
-                    <a href="fees.html"><i class="fas fa-comment-dollar"></i> <span>Fees</span></a>
-                </li>
-                <li>
-                    <a href="exam.html"><i class="fas fa-clipboard-list"></i> <span>Exam list</span></a>
-                </li>
-                <li>
-                    <a href="event.html"><i class="fas fa-calendar-day"></i> <span>Events</span></a>
-                </li>
-                <li>
-                    <a href="time-table.html"><i class="fas fa-table"></i> <span>Time Table</span></a>
-                </li>
-                <li>
-                    <a href="library.html"><i class="fas fa-book"></i> <span>Library</span></a>
-                </li>
-                <li class="submenu">
-                    <a href="#"><i class="fa fa-newspaper"></i> <span> Blogs</span>
-                        <span class="menu-arrow"></span>
-                    </a>
-                    <ul>
-                        <li><a href="blog.html">All Blogs</a></li>
-                        <li><a href="add-blog.html">Add Blog</a></li>
-                        <li><a href="edit-blog.html">Edit Blog</a></li>
-                    </ul>
-                </li> --}}
 
                 <li class="{{ request()->is('admin/settings*') ? 'active' : '' }}">
                     <a href="{{ url('admin/settings/general-settings') }}"><i class="fas fa-cog"></i>
